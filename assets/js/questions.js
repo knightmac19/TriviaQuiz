1b609ebaa8ad8644b277d1ffbcf668c0cc4b1d25
Patrick Dunn <pmdunn78@gmail.com>
2021-01-22 22:07:20 -0600

    added questions file with questions arr

---
+++ assets/js/questions.js
@@ -0,0 +1,128 @@
+const questions = [
+    {
+        text: "How many US state capitols are west of Los Angeles?",
+        options: [
+            {
+                choice: "0",
+                correct: false
+            },
+            {
+                choice: "3",
+                correct: false
+            },
+            {
+                choice: "6",
+                correct: true
+            },
+            {
+                choice: "1",
+                correct: false
+            }
+        ]
+    },
+    {
+        text: "Which is the second-largest country of Spanish speakers?",
+        options: [
+            {
+                choice: "Colombia",
+                correct: false
+            },
+            {
+                choice: "Spain",
+                correct: false
+            },
+            {
+                choice: "Mexico",
+                correct: false
+            },
+            {
+                choice: "United States",
+                correct: true
+            }
+        ]
+    },
+    {
+        text: "What international event is celebrated every April 22nd?",
+        options: [
+            {
+                choice: "Earth Day",
+                correct: true
+            },
+            {
+                choice: "Marijuana Day",
+                correct: false
+            },
+            {
+                choice: "Pandora Day",
+                correct: false
+            },
+            {
+                choice: "Limerick Day",
+                correct: false
+            }
+        ]
+    },
+    {
+        text: "Which beloved 'Jeopardy' host passed away in 2020?",
+        options: [
+            {
+                choice: "Ken Jennings",
+                correct: false
+            },
+            {
+                choice: "Pat Sajak",
+                correct: false
+            },
+            {
+                choice: "Alex Trebek",
+                correct: true
+            },
+            {
+                choice: "Vanna White",
+                correct: false
+            }
+        ]
+    },
+    {
+        text: "HTML stands for...?",
+        options: [
+            {
+                choice: "Hot & Tangy Meat Loaf",
+                correct: false
+            },
+            {
+                choice: "HyperText Markup Language",
+                correct: true
+            },
+            {
+                choice: "Hybrid Transverse Media Language",
+                correct: false
+            },
+            {
+                choice: "How The Mandalorian Leaps",
+                correct: false
+            }
+        ]
+    },
+    {
+        text: "How do you comfort a JavaScript bug?",
+        options: [
+            {
+                choice: "You bring it flowers",
+                correct: false
+            },
+            {
+                choice: "You hug it",
+                correct: false
+            },
+            {
+                choice: "You log it",
+                correct: false
+            },
+            {
+                choice: "You console it",
+                correct: true
+            }
+        ]
+    }  
+];
---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    console.log( "I'm ready!" );
+    
+    for (var i = 0; i < questions.length; i++) {
+        console.log(questions[i].text);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
