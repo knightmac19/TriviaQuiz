a0711b4b1a890228f6f8dd8adbe9c35634773001
Patrick Dunn <pmdunn78@gmail.com>
2021-01-22 22:32:59 -0600

    start btn init & text counter call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,52 @@
 $(document).ready(function() {
     console.log( "I'm ready!" );
+
+    var counter = $('#counter');
+    var startBtn = $('#start-btn');
+    let score = 0;
+
+    function hideStartBtn() {
+        // define visible / invisible css classes
+        // call appropriate class
+    };
+
+    function showStartBtn() {
+        // define visible / invisible css classes
+        // call appropriate class
+    };
+
+    function showQuestions() {
+        // define visible / invisible css classes
+        // call appropriate class
+    };
+
+    function hideQuestions() {
+        // define visible / invisible css classes
+        // call appropriate class
+    };
+
     
-    for (var i = 0; i < questions.length; i++) {
-        console.log(questions[i].text);
+
+    function initGame() {
+        hideStartBtn();
+        counter.text('Time Left: 00:59');
+
+        showQuestions();
+        for (var i = 0; i < questions.length; i++) {
+            console.log(questions[i].text);
+        }
     }
     
+    
+
+
+
+
+
+    startBtn.on('click', function() {
+        initGame();
+    });
+
 
 
 
